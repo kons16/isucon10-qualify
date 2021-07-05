08dc4a3eee8397c1f1d8df6b3abb515858e23090
kons16 <river.field.1126@gmail.com>
2021-07-05 10:01:34 +0000

    Fix nginx.conf, bench shell

--- isuumo/bench/restart-and-bench.sh
+++ isuumo/bench/restart-and-bench.sh
@@ -8,5 +8,8 @@ sleep 5
 
 echo | sudo tee /var/log/nginx/access.log
 
-./bench
-cat /var/log/nginx/access.log | alp ltsv --sort avg -r
+./bench -target-url http://127.0.0.1:8080
+chmod 777 /var/log/nginx/access.log
+rm alp_kekka.txt
+/var/log/nginx/access.log | alp ltsv --sort avg -r >> alp_kekka.txt
+
--- isuumo/setup.sh
+++ isuumo/setup.sh
@@ -4,3 +4,8 @@ unzip alp_linux_amd64.zip
 rm alp_linux_amd64.zip
 mv alp /usr/local/bin/
 
+# nginxをgit管理下に設定する
+cp -r /etc/nginx /home/isucon/nginx
+rm -rf /etc/nginx/
+ln -s /home/isucon/nginx /etc/nginx
+
