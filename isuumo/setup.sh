c0707ccdda34cfbddaac2bbdf4837c0978d0453b
kons16 <ks1216r@gmail.com>
2021-07-09 16:52:33 +0900

    Add grafana, promentheus in setup.sh

--- isuumo/setup.sh
+++ isuumo/setup.sh
@@ -1,11 +1,46 @@
-# alp
+#!/bin/bash
+### 初期セットアップ用
+set -e
+
+apt-get install unzip
+
+### alp
+echo "starting alp install...."
 wget https://github.com/tkuchiki/alp/releases/download/v1.0.3/alp_linux_amd64.zip
 unzip alp_linux_amd64.zip
 rm alp_linux_amd64.zip
 mv alp /usr/local/bin/
 
-# nginxをgit管理下に設定する
+### prometheus
+echo "starting prometheus install...."
+sudo apt-get update
+apt-get install -y prometheus prometheus-node-exporter
+ufw allow 9090
+systemctl start prometheus.service
+systemctl start prometheus-node-exporter-apt.service
+systemctl start prometheus-node-exporter-ipmitool-sensor.service
+systemctl start prometheus-node-exporter-mellanox-hca-temp.service
+systemctl start prometheus-node-exporter-smartmon.service
+systemctl start prometheus-node-exporter.service
+
+### Grafana
+echo "starting Grafana install...."
+apt-get install -y apt-transport-https
+apt-get install -y software-properties-common wget
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+apt-get update
+apt-get install -y grafana
+ufw allow 3000
+systemctl start grafana-server
+### CPU使用率 Query : rate(node_cpu_seconds_total{job="prometheus",mode="system"}[5m]) * 100
+### メモリ使用率　Wuery : node_memory_MemTotal_bytes{instance="localhost:9100",job="prometheus"}
+
+
+### リポジトリに nginx のシンボリックリンクを貼る
+echo "nginx のシンボリックリンクを作成中"
 cp -r /etc/nginx /home/isucon/nginx
 rm -rf /etc/nginx/
 ln -s /home/isucon/nginx /etc/nginx
 
+### pt-query-digest
